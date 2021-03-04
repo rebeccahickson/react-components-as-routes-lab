5e4df959ecb1e284d734ccec22eb9b3fc78c5892
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-04 15:24:15 -0500

    commit tree bbfe46de4c48c1f7e2405f15a06a60a52556acf2

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((director) => (
+        <div>
+          {director.name}
+          <ul>
+            {director.movies.map((movie) => (
+              <li>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie) => (
+        <div>
+          {movie.title}
+          {movie.time}
+          <ul>
+            {movie.genres.map((genre) => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
